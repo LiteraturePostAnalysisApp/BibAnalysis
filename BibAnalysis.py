775ba63487b60313ea253a6f18d99a067dc52368
ganansuan647 <1852992@tongji.edu.cn>
2024-05-27 10:45:53 +0800

    change class name

--- BibAnalysis.py
+++ BibAnalysis.py
@@ -1,12 +1,13 @@
 from tinydb import TinyDB,Query
 from config import MAPTIER_API_KEY
 from cerberus import Validator
-from validaterules import validate_rules
+# from validaterules import validate_rules
 from excel_interaction import ExcelInteraction
 import json
 import os
 import requests
 import re
+from rich import print
 
 class BibAnalysis:
     def __init__(self, *args, **kwargs):
@@ -18,5 +19,14 @@ class BibAnalysis:
         """
         self.bib_database_path = kwargs.get('bib_database_path', './data/bib_database.json')
         self.database = TinyDB(self.bib_database_path)
+
+    def __str__(self):
+        """Return the string representation of the class."""
+        return f'bib class at path:{self.bib_database_path}\n have {len(self.database)} articles in database.'
     
-    
+
+if __name__ == '__main__':
+    bib = BibAnalysis()
+    print(bib.bib_database_path)
+    print(bib)
+    print('BibAnalysis class is working fine.')
